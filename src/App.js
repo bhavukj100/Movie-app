5e4fce68d7827ceb16eaeaaaa6df988f303c596a
bhavukj100 <bhavukj100@gmail.com>
2022-01-31 04:24:15 +0530

    adding new files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
+import React from "react";
 import './App.css';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Header } from "./Header";
-import { useEffect, useState } from "react";
-import { Wishlist } from './Wishlist';
-import { LoginApp } from './Login'; 
+//import { Header } from "./Header";
+//import { useEffect, useState } from "react";
+//import { Wishlist } from './Wishlist';
+//import { LoginApp } from './Login'; 
 import { Home } from './Home';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase';
+ import Dashboard from "./dashboard";
+import LogIn from "./Login";
 
-import './App.css';
-import Form from "./Login"
- 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Form />
-    </div>
-     
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/login" component={LogIn} />
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
-}
- 
+};
+
 export default App;
